c268b7a8a8a87252fb6593b6a997c5bc5b3fedef
fernanda-vasilica <79586541+fernanda-vasilica@users.noreply.github.com>
2021-04-06 19:18:39 +0300

    Add files via upload

---
+++ Tema2_P2/PatientTemp.java
@@ -0,0 +1,16 @@
+package Tema2_P2;
+
+public class PatientTemp {
+
+	final int id;
+	final double temp;
+	public PatientTemp (int id, double temp) {
+		super();
+		this.id = id;
+		this.temp = temp;
+		
+		
+	}
+	
+	
+}
